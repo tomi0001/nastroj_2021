e590eba04c36c0fb18d2b129d6345433761d6cb4
tomi0001 <tomi0001@gmail.com>
2022-01-26 17:56:28 +0100

    ustawienia uzytkownika

--- public/styles/default/js/pageSetting.js
+++ public/styles/default/js/pageSetting.js
@@ -2,6 +2,8 @@ function loadPageMood() {
     $(".titleSettingsMood").addClass("selectedMenu");
     $(".titleSettingsDrugs").removeClass("selectedMenu");
     $(".titleSettingsUser").removeClass("selectedMenu");
+    $("#MenuPageMood").css("display","block");
+    $("#MenuPageDrugs").css("display","none");
     
 }
 
@@ -10,10 +12,21 @@ function loadPageDrugs() {
     $(".titleSettingsMood").removeClass("selectedMenu");
     $(".titleSettingsDrugs").addClass("selectedMenu");
     $(".titleSettingsUser").removeClass("selectedMenu");    
+    $("#MenuPageDrugs").css("display","block");
+    $("#MenuPageMood").css("display","none");
 }
 
 function loadPageUser() {
     $(".titleSettingsMood").removeClass("selectedMenu");
     $(".titleSettingsDrugs").removeClass("selectedMenu");
     $(".titleSettingsUser").addClass("selectedMenu");    
+}
+
+
+function selectMenuMood(menu) {
+    $("#" + menu).addClass("selectedMenuMoodHref");
+}
+
+function unSelectMenuMood(menu) {
+    $("#" + menu).removeClass("selectedMenuMoodHref");
 }
--- resources/views/Users/Settings/main.blade.php
+++ resources/views/Users/Settings/main.blade.php
@@ -9,19 +9,57 @@
 @endsection
 
 <br>
-<div class="titleSettings">USTAWIENIA UŻYTKOWNIKA</div>
+<div class="titleSettings">USTAWIENIA KONTA</div>
 <div class="titleAllSettings">
     <a class="hrefSettingCursor" onclick="loadPageMood()"><div class="titleSettingsMood titleSettingsAll">USTAWIENIA NASTROJU</DIV></a>
     <a class="hrefSettingCursor" onclick="loadPageDrugs()"><div class="titleSettingsDrugs titleSettingsAll">USTAWIENIA PRODUKTÓW</DIV></a>
     <a class="hrefSettingCursor" onclick="loadPageUser()"><div class="titleSettingsUser titleSettingsAll">USTAWIENIA UŻYTKOWNIKA</DIV></a>
 </div>
-<div id="MenuPageMood" style="display: none;">
-    
-</div>
-<div id="MenuPageDrugs" style="display: none;">
-    ss
-</div>
-<div id="MenuPageUser" style="display: none;">
-    
+<div class="downPage">
+    <div id="MenuPageMood" class="pagepage pageMood" style="display: none;">
+       
+           
+            <div id="addAction" class="hrefMood hrefSettingCursor" onmouseover="selectMenuMood('addAction')" onmouseout="unSelectMenuMood('addAction')">
+               DODAJ NOWĄ AKCJE
+            </div>
+           
+            <div id="levelMood"  class=" hrefMood hrefSettingCursor" onmouseover="selectMenuMood('levelMood')" onmouseout="unSelectMenuMood('levelMood')">
+                POZIOMY NASTROJU
+            </div>
+            <div id="changeNameAction"  class=" hrefMood hrefSettingCursor" onmouseover="selectMenuMood('changeNameAction')" onmouseout="unSelectMenuMood('changeNameAction')">
+                ZMIEŃ NAZWY AKCJI
+            </div>
+            <div id="changeDateAction"  class=" hrefMood hrefSettingCursor" onmouseover="selectMenuMood('changeDateAction')" onmouseout="unSelectMenuMood('changeDateAction')">
+                ZMIEŃ DATY AKCJI
+            </div>
+        
+    </div>
+    <div id="MenuPageDrugs" class="pagepage pageDrugs" style="display: none;">
+            <div id="newGroup" class="hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('newGroup')" onmouseout="unSelectMenuMood('newGroup')">
+               DODAJ NOWĄ GRUPĘ
+            </div>
+           
+            <div id="newSubstance"  class=" hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('newSubstance')" onmouseout="unSelectMenuMood('newSubstance')">
+                DODAJ NOWĄ SUBSTANCJĘ
+            </div>
+            <div id="newProduct"  class=" hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('newProduct')" onmouseout="unSelectMenuMood('newProduct')">
+                DODAJ NOWY PRODUKT
+            </div>
+            <div id="editGroup"  class=" hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('editGroup')" onmouseout="unSelectMenuMood('editGroup')">
+                EDYTUJ GRUPĘ
+            </div>
+            <div id="editSubstance"  class=" hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('editSubstance')" onmouseout="unSelectMenuMood('editSubstance')">
+                EDYTUJ SUBSTANCJĘ
+            </div>
+            <div id="editProduct"  class=" hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('editProduct')" onmouseout="unSelectMenuMood('editProduct')">
+                EDYTUJ PRODUKT
+            </div>
+            <div id="planedDose"  class=" hrefDrugs hrefSettingCursor" onmouseover="selectMenuMood('planedDose')" onmouseout="unSelectMenuMood('planedDose')">
+                ZAPLANUJ DAWKĘ
+            </div>
+    </div>
+    <div id="MenuPageUser" style="display: none;">
+
+    </div>
 </div>
 @endsection
