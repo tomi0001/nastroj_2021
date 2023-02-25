b039dc5dab17aad50909462e9ab19e90e3303d0e
tomi3 <tomi3@tomi3-VivoBook-15-ASUS-Laptop-X542UF>
2023-02-25 15:36:50 +0100

    poprawka drobna

--- resources/views/ajax/showAverage.blade.php
+++ resources/views/ajax/showAverage.blade.php
@@ -9,18 +9,24 @@
 </div>
 <select id='averageType{{$id}}' class='form-control' onchange="loadAverage('{{ route('ajax.sumAverage')}}',{{$id}})">
     <option value="" selected></option>
+    <optgroup label="Produkty">
     @foreach ($productName as $list)
+      
         <option value="1_{{$list->id_products}}" >dla {{$list->nameProducts}}</option>
-
+      
     @endforeach
-    
+    </optgroup>
+    <optgroup label="Substancje">
     @foreach ($allSubstance as $list)
-        
+      
         <option value="2_{{$list->id_substances}}" >dla {{$list->nameSubstances}}</option>
-
+      
     @endforeach
+    </optgroup>
     @if ($Equivalent != null) 
+      <optgroup label="Benzodiazepiny">
         <option value="3_{{$Equivalent}}" >Równoważnik diazepamu 10 mg</option>
+      </optgroup>
     @endif
 </select>
 
