0e56dc68674bbad12b3e7d24520c2a15080d7a2e
tomi3 <tomi3@tomi3-VivoBook-15-ASUS-Laptop-X542UF>
2023-12-18 16:32:05 +0100

    poprawki

--- config/view.php
+++ config/view.php
@@ -12,7 +12,7 @@ return [
     | the usual Laravel view path has already been registered for you.
     |
     */
-    'styles_color' => 'autumn',
+    'styles_color' => 'winter',
     'styles' => 'default',
     'paths' => [
         resource_path('views'),
